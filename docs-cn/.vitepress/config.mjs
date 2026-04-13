bc45a97b2de70816584f3b219546b14a72c129c9
Jump Ten Wong <awong1900@163.com>
2026-04-13 23:16:13 +0800

    fix: docs cn

---
+++ docs-cn/.vitepress/config.mjs
@@ -0,0 +1,85 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  // title: "IORI Timer",
+  // description:
+  //   "Simple Yet Powerful Timer for Soccer Training and Video Recording",
+  locales: {
+    root: {
+      label: "中文",
+      lang: "zh-CN",
+      title: "IORI计时器",
+      themeConfig: {
+        nav: [
+          { text: "主页", link: "/" },
+          { text: "指南", link: "/guide/" },
+          // {
+          //   text: "型号",
+          //   ariaLabel: "Language Menu",
+          //   items: [
+          //     { text: "IORI 1S", link: "/cn/iori-1/" },
+          //     { text: "IORI PRO", link: "/cn/iori-pro/" },
+          //   ],
+          // },
+          // {
+          //   text: "测试教案",
+          //   link: "/drill/",
+          // },
+          // { text: "购买", link: "/guide.html#官方购买渠道" },
+          {
+            text: "评测合集",
+            link: "https://v.douyin.com/iP3phvVo/",
+          },
+          // {
+          //   text: "教练试用申请",
+          //   link: "https://docs.qq.com/form/page/DUmFoaXlCQnZQU3Zz",
+          // },
+        ],
+        socialLinks: [
+          {
+            icon: "tiktok",
+            link: "https://v.douyin.com/iP3Gg77v/",
+          },
+        ],
+        sidebar: [
+          {
+            text: "主页",
+            link: "/cn",
+          },
+          {
+            text: "指南",
+            link: "/cn/guide",
+          },
+        ],
+        lastUpdated: {
+          text: "最近更新",
+        },
+        footer: {
+          copyright: "Copyright © 2021-2025 一起踢足球训练营",
+        },
+        sidebarMenuLabel: "菜单",
+        darkModeSwitchLabel: "外观",
+        outline: {
+          label: "页面导航",
+        },
+        docFooter: {
+          prev: "上一页",
+          next: "下一页",
+        },
+        returnToTopLabel: "返回顶部",
+      },
+    },
+  },
+  cleanUrls: true,
+  lastUpdated: true,
+  themeConfig: {
+    logo: "/logo.png",
+    // search: {
+    //   provider: "local",
+    // },
+  },
+  sitemap: {
+    hostname: "https://ioritimer.com",
+  },
+});
